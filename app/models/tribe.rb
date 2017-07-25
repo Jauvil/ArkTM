0656d407165d11e69ea6010d292625acdf40da85
Jauvil <justinauvil15@gmail.com>
2017-07-25 16:39:12 -0400

    Took me all damn day but finally figured out I need to use group_by method before the map method to get eval i wanted.

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -20,107 +20,52 @@ class Tribe < ApplicationRecord
   end
 
   def opt_breed_pair(breed)
-    male_hash = self.best_stats(breed)
-    female_hash = self.best_stats(breed)
-    if female_hash.present?
-      sorted_female_hash = female_hash.sort_by{|_, v| v}.reverse
-      best_female = Dino.where(breed: breed, name: sorted_female_hash[0][0]).first
+    best_dinos_array = self.best_stats(breed)
+    compiled_stats = best_dinos_array.group_by(&:first).map { |k, vs| [k, vs.map(&:last)]}
+    compiled_stats.each do |dino_array|
+      puts dino_array[0].name + dino_array[0].gender
     end
-    if male_hash.present?
-      sorted_male_hash = male_hash.sort_by{|_, v| v}.reverse
-      best_male = Dino.where(breed: breed, name: sorted_male_hash[0][0]).first
-    end
-    return [best_male, best_female]
+
   end
 
   def best_stats(breed)
     same_breed = Dino.dinos_by_breed(self, breed)
-    max_health_object = self.max_health_dino(same_breed)
-    counts_male = Hash.new(0)
-    counts_female = Hash.new(0)
-    if max_health_object.gender == 'Male' #### 1 if best health dino M, look for best stam
-      best_health_dino = max_health_object ### 1
-      females = same_breed.female_dinos ### 1
-      best_stam_dino = self.max_stamina_dino(same_breed) ###
-      if best_stam_dino == best_health_dino #### if stam dino is same as health dino
-        counts_male[best_health_dino.name] += 2
-        best_oxygen_dino = self.max_oxygen_dino(same_breed)
-        if best_oxygen_dino == best_health_dino #### if oxygen dino is same as stam dino is same as health
-          counts_male[best_health_dino.name] += 1 #### add to counts for health dino
-        else
-          counts_male[best_oxygen_dino.name] += 1
-          best_oxygen_dino = self.max_oxygen_dino(females) #### if oxygen isnt same as health, stam
-          counts_female[best_oxygen_dino.name] += 1
-          best_food_dino = self.max_food_dino(same_breed)
-          if best_food_dino == best_oxygen_dino
-            counts_female[best_oxygen_dino.name] += 1
-          elsif best_food_dino == best_health_dino
-            counts_male[best_health_dino.name] += 1
-          elsif best_food_dino.gender == 'Male'
-            counts_male[best_food_dino.name] += 1
-          end
-        end
-      elsif best_stam_dino.gender == 'Male'
-        best_stam_dino = self.max_stamina_dino(females)
-      end
-      best_oxygen_dino = self.max_oxygen_dino(same_breed) #### 1 Look for all oxgen dinos
-      if best_oxygen_dino == best_health_dino ####  2 if oxygen dino is health dino add to its count
-        counts[best_health_dino.name] += 1 ### 2
-        best_food_dino = self.max_food_dino(females) #### 2 if oxy dino is health dino find best food dino
-        if best_food_dino == best_health_dino #### 3 if food dino is health dino add to health dino count
-          counts[best_health_dino.name] += 1 ### 3
-        end
-      elsif best_oxygen_dino == best_stam_dino #### if oxygen dino is same as stam dino add to its count
-        counts[best_stam_dino.name] += 1
-      else
-
-      end
-    else
-      ### if max health object is female
-      best_health_dino = max_health_object
-      males = same_breed.male_dinos
-      best_stam_dino = self.max_stamina_dino(males)
-      if best_stam_dino == best_health_dino
-
-      end
-    end
-    max_oxygen_object = self.max_oxygen_dino(same_breed)
-    max_food_object = self.max_food_dino(same_breed)
-    max_weight_object = self.max_weight_dino(same_breed)
-    max_melee_object = self.max_melee_dino(same_breed)
-
-
-      return [counts_male, counts_female]
-    end
+    health = self.max_health_dino(same_breed)
+    stamina = self.max_stamina_dino(same_breed)
+    oxygen = self.max_oxygen_dino(same_breed)
+    food = self.max_food_dino(same_breed)
+    weight = self.max_weight_dino(same_breed)
+    melee = self.max_melee_dino(same_breed)
+    [health, stamina, oxygen, food, weight, melee]
   end
 
   def max_health_dino(dino_list)
     max_health_value = dino_list.maximum(:health)
-    dino_list.find_by_health(max_health_value)
+    return [dino_list.find_by_health(max_health_value), "health"]
   end
 
   def max_stamina_dino(dino_list)
     max_stamina_value = dino_list.maximum(:stamina)
-    dino_list.find_by_stamina(max_stamina_value)
+    return [dino_list.find_by_stamina(max_stamina_value), "stamina"]
   end
 
   def max_oxygen_dino(dino_list)
     max_oxygen_value = dino_list.maximum(:oxygen)
-    dino_list.find_by_oxygen(max_oxygen_value)
+    return [dino_list.find_by_oxygen(max_oxygen_value), "oxygen"]
   end
 
   def max_food_dino(dino_list)
     max_food_value = dino_list.maximum(:food)
-    dino_list.find_by_food(max_food_value)
+    return [dino_list.find_by_food(max_food_value), "food"]
   end
 
   def max_weight_dino(dino_list)
     max_weight_value = dino_list.maximum(:weight)
-    dino_list.find_by_weight(max_weight_value)
+    return [dino_list.find_by_weight(max_weight_value), "weight"]
   end
 
   def max_melee_dino(dino_list)
     max_melee_value = dino_list.maximum(:melee)
-    dino_list.find_by_melee(max_melee_value)
+    return [dino_list.find_by_melee(max_melee_value), "melee"]
   end
 end
