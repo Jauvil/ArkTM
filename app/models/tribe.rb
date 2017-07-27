67c83603c03313080e0e5e56293eccfd30d930e0
Jauvil <justinauvil15@gmail.com>
2017-07-27 15:44:22 -0400

    removed mailer until i can figure it out the right way

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -4,7 +4,6 @@ class Tribe < ApplicationRecord
   has_many :dinos
 
   def tribe_join_request(user)
-    UserMailer.tribe_request(user).deliver_now
     TribeRequest.create!(user_id: user.id, tribe_id: self.id)
   end
 
